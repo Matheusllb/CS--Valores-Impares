0ec8eb991a272897bb5c081d5f3d5912fb0271f1
Matheusllb <matheusluislopesbatista@gmail.com>
2023-04-04 20:18:58 -0300

    Encontrando valores ímpares

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace CsE18
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCÍCIO 18
+            Console.Write("Encontrando valores ímpares\n\nInforme um valor: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
